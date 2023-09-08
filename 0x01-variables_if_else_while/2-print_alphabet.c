b47f07cc7519db7b8baaa2e898cde6228508f82c
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-08 14:34:41 -0700

    Low Level Programing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+/*print_alphabet.c
+ * AUTHOR: Jossefa Alberto Muchanga
+ */
+
+void main(){
+
+	char let;
+	for(let='a';let<='z';let++)
+		putchar(let);
+
+	putchar('\n');
+	return (0);
+}
