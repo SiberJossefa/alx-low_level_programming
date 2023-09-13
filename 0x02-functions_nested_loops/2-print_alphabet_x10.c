84871830341dfa8baccc8c7f6fca015de8c26ca0
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-13 14:10:53 -0700

    Functions Nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,14 @@ void print_alphabet_x10(void)
 {
 char ch;
 int j=0;
-while(j<10)
-{
-for(ch = 'a';ch<='z';ch++)
-{
-_putchar(ch);
-}
-_putchar('\n');
-j++;
-}
+	while(j<10)
+             {	
+		for(ch = 'a';ch<='z';ch++)
+	        {
+			_putchar(ch);
+	        }
+		_putchar('\n');
+		j++;
+	     }
 
 }
