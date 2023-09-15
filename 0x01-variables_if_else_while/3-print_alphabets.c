ad83244405a11bc5dfef51ae647924fcfbd92366
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-15 09:37:05 -0700

    Low Level Programing

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,23 @@
 #include<stdlib.h>
 
 
-/*print_alphabets.c
+/*
+ * print_alphabets.c
  * AUTHOR: Jossefa Alberto Muchanga
  */
 
 int main(void)
 {
 
-   char let;
-   for(let='a';let<='z';let++)
-	   putchar(let);
-   for(let='A';let<='Z';let++)
-	   putchar(let);
+	char let;
+   
+ 	for (let = 'a'; let <= 'z'; let++)
+		putchar(let);
 
-   putchar('\n');
-   return (0);
+	for (let = 'A'; let <= 'Z'; let++)
+		putchar(let);
+
+	putchar('\n');
+   
+	return (0);
 }
