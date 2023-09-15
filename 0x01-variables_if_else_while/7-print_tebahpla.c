5952ceab7cfa3d3717ba82108197be16f16b94a3
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-15 09:48:12 -0700

    Low Level Programing

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
-
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
 
 int main(void)
 {
 	char let;
-	for(let='z';let>='a';let--)
+
+	for (let = 'z'; let >= 'a'; let--)
 		putchar(let);
-	putchar('n');
-	return(0);
+
+	putchar('\n');
+
+	return (0);
 }
