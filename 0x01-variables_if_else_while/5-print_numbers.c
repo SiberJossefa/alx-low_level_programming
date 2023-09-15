9ff7e498ee63cb71c795e7cb2b92a1edc90d4012
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-15 10:32:07 -0700

    Low Level Programing

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
 int main(void)
 {
 	int number;
-	for (number=0;number<10;number++)
-		printf("%i",number);
+
+	for (number = 0; number < 10; number++)
+		printf("%i", number);
 	printf("\n");
+
 	return (0);
 }
