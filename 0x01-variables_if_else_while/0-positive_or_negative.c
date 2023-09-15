ff8a263cf694bc4f1adfd0bec28f0832a727456a
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-15 06:58:54 -0700

    Low Level Programing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 #include<time.h>
 
-/*positive_or_negative.c
+/*
+ * positive_or_negative.c
  * AUTHOR: Jossefa Alberto Muchanga
  */
 
@@ -11,14 +12,14 @@ int main (void)
 
 int n;
 srand(time(0));
-n = rand()-RAND_MAX/2;
+n = rand() - RAND_MAX / 2;
 
-if ( n > 0 )
-	printf ("%i is positive\n", n);
+	if ( n > 0 )
+		printf ("%i is positive\n", n);
 	else if ( n < 0 )
 		printf("%i is negative\n", n);
-else
-	printf("%i is Zero\n", n);
+	else
+		printf("%i is Zero\n", n);
 
 return (0);
 }
