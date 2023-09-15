51b3d985a056636e6d748ee71a2217f5d4db4dab
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-15 10:41:17 -0700

    Low Level Programing

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-        int number;
+	int number;
    
-   	for (number = 0; number < 10; number++)
+	for (number = 0; number < 10; number++)
 		putchar((number % 10) + '0');
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
