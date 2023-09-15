0b931389b69182128c64d5ea015a555c89020fd1
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-15 10:35:52 -0700

    Low Level Programing

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
 int main(void)
 {
         int number;
-        for (number=0;number<10;number++)
-                putchar((number%10)+'0');
+   
+   	for (number = 0; number < 10; number++)
+                putchar((number % 10) + '0');
         putchar('\n');
         return (0);
 }
