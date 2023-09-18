bf6ed5d79f1413755f423282dab80a90110970a8
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-18 05:20:46 -0700

    Functions Nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,12 @@ void print_alphabet_x10(void)
 	int j = 0;
 
 	while (j < 10)
-             {	
+	{	
 		for (ch = 'a'; ch <= 'z'; ch++)
-	        {
+		{
 		_putchar(ch);
-	        }
+		}
 		_putchar('\n');
 		j++;
-	     }
-
+	}
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include"main.h"
 
 /**
-*islower
-*Return;1 charater or 0 anything
+ * main - entry point
+ *
+*Return: 1 charater or 0 anything
 */
 
 int _islower(int c)
 {
-	if (c>=97 && c <=122)
+	if (c >= 97 && c <= 122)
 	{
 	return (1);
 	}
