6a3b5985ad5eddcd6ff057dd502d1dc4e3a79961
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-22 00:01:15 -0700

    More Functions Nested Loop

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - from 0 to 9
+ * @j: intenger
+ */
+void print_numbers(void)
+{
+	int j = 0;
+
+	for (j = 0; j <= 9; j++)
+	{
+		_putchar(j);
+	_putchar('\n');
+	}
+}
