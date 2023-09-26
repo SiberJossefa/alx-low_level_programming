31d251e49b7c7020e7ea7095d7991e7d03d7a5f7
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-26 00:39:58 -0700

    More Functions Nested Loop

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	char i,j = 0;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+			putchar((j / 10) + '0');
+			}
+			putchar((j % 10) + '0');
+		}
+		putchar('\n');
+	
+	}
+}
