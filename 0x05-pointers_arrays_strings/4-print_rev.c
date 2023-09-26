b7ce33eb1d32bbc638f0b36610e58cef711a86bc
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-26 16:04:29 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - print in reverse
+ * @s: string
+ *return: 0
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int d;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	s--;
+	for (d = l; d > 0; d--)
+	{
+		putchar(*s);
+		s--;
+	}
+	putchar('\n');
+
+	return (0);
+}
