549c717de4e8348e1a8d65eba3208d9457478ff2
SiberJossefa <jossefamuchanga891@gmail.com>
2023-09-26 06:24:52 -0700

    More Functions Nested Loop

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * void print_square - prints a square
+ */
+void print_square(int size)
+{
+	char a, b;
+	
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
