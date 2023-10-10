d0a3f6342e250f121ffcc3262d6d0ca08497295d
SiberJossefa <jossefamuchanga891@gmail.com>
2023-10-10 02:44:24 -0700

    Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to  be printrd
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(S + 1);
+		_putchar(*s);
+	}
+}
