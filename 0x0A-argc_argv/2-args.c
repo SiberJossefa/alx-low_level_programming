cc8de451502d8d0f1bd76fa9e6796139638d1e64
SiberJossefa <jossefamuchanga891@gmail.com>
2023-11-01 15:25:01 -0700

    Arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function that prints all arguments its receive
+ *@argc: receive parameter intenger
+ *@argv: receive parameter string
+ *
+ * Return: always 0 sucess
+ */
+int main(int argc, char *argv[])
+{
+        int j;
+
+        for (j = 0; j < argc; j++)
+        {
+                printf("%s\n", argv[j]);
+        }
+        return (0);
+}
