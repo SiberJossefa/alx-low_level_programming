7f151c07045088a20fd7c831bc76dded4ba3552c
SiberJossefa <jossefamuchanga891@gmail.com>
2023-11-02 06:16:55 -0700

    Arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/*
+ * main - adds positive numbers
+ * @argc: argument
+ * @argv: argument
+ *
+ * Return: 0 sucess, 1 
+ */
+int main(int argc, char *argv[])
+{
+	char *p;
+	int s;
+
+	if (argc - 1 == 0)
+	{
+		printf("0\n");
+	}
+	s = 0;
+	argv++;
+	while (*argv)
+	{
+		p = *argv;
+
+		while (*p)
+		{
+			if (!isdigit(*p))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			p++;
+		}
+		s += atoi(*argv++);
+	}
+	printf("%i\n", s);
+	return (0);
+}
