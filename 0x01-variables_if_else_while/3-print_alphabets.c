81705fad2902ea7deb3fcebe04fa04129bd1762c
Siberjossefa <jossefamuchanga891@gmail.com>
2025-03-15 10:55:13 -0700

    Low Level

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 	char let;
-   
- 	for (let = 'a'; let <= 'z'; let++)
+	
+	for (let = 'a'; let <= 'z'; let++)
 		putchar(let);
 
 	for (let = 'A'; let <= 'Z'; let++)
 		putchar(let);
 
 	putchar('\n');
-   
+
 	return (0);
 }
