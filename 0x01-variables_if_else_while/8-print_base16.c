7aeb241d2003606b1f9216bd802942078f0b3a84
Siberjossefa <jossefamuchanga891@gmail.com>
2025-03-16 08:48:24 -0700

    Low Level

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * retund: 0 always
+ */
+int main(void)
+{
+	char num = 0;
+	char let = 'a';
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	while (let <= 'f')
+	{
+		putchar(let);
+		let++;
+	}
+	putchar('\n');
+
+	return (0);
+}
