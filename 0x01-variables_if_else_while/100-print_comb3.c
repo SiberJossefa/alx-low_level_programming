afc1117b8ffb71550ebd67e7b701a2653edcb3af
Siberjossefa <jossefamuchanga891@gmail.com>
2025-03-18 09:57:12 -0700

    Low Level

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int cont = 1;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (i = 0 + 1; j <= 9; j++)
+		{
+			if (cont == 0)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(i + '0');
+			putchar(j + '0');
+			cont = 0;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
