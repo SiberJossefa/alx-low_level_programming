7f395b72969e548aff4c9375d04b6ad71da6a7dd
Siberjossefa <jossefamuchanga891@gmail.com>
2025-03-24 11:47:07 -0700

    Low Level Programing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,23 @@
 #include "main.h"
 /**
-*print_sign: prints the sign of any number
-*@n: intenger number
-*return: 1 if n is great than 0
-*/
-int print_sign(int n) 
+ *print_sign: prints the sign of any number
+ *@n: intenger number
+ *
+ *Return: 1 if n is great than 0
+ */
+int print_sign(int n)
 {
 	if (n > 0)
-	 {
-	 _putchar('+');
-	 return (1);
-	 }
-	 else if (n < 0)
-	 {
-	 _putchar('-');
-	 return (-1);
-	 }
-	 
-	 _putchar('0');
-	 return (0);	 
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return(-1);
+	}else{
+		_putchar('0');
+		return(0);
+	}
 }
