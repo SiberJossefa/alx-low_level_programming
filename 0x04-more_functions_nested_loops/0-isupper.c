3d4936689ebe8ad00f90a45f3bf7a45671638b3a
Siberjossefa <jossefamuchanga891@gmail.com>
2025-06-25 03:20:28 -0700

    Uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - Functon that check uppercase character
  * @c: intenger
- * Return - 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
